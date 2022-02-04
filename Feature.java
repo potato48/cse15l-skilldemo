869d675dca16161d68058783f848f135a98a86d5
potato48 <kevin.liu909@gmail.com>
2022-02-04 15:13:30 -0800

    Start

---
+++ Feature.java
@@ -0,0 +1,5 @@
+public class Feature {
+    public static int doMath(int n) {
+        return n * n;
+    }
+}
---
+++ FeatureTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class FeatureTester {
+    @Test
+    public void testDoMath() {
+        assertEquals(4, Feature.doMath(1));
+    }
+}
